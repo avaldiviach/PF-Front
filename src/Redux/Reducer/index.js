3766c52fb7fa705cf1417931413bd6ac147da58e
avaldiviach <95005227+avaldiviach@users.noreply.github.com>
2022-05-31 03:46:06 -0500

    Update index.js

--- src/Redux/Reducer/index.js
+++ src/Redux/Reducer/index.js
@@ -60,7 +60,8 @@ const initialState = {
 	orderById: [],
 	getRole: '',
 	getToken: '',
-	getUser: null
+	getUser: null,
+	offer: [{ id: 13, size: 37.5 }]
 
 };
 
@@ -121,16 +122,12 @@ const rootReducer = (state = initialState, { type, payload }) => {
 		case CLEAN_DETAIL: return { ...state, detail: [] }
 
 		case SORT_PRICE:
-
 			let sortByPrice = [...state.SneakersCopy];
-			console.log(sortByPrice)
-
 			if (payload === 'asc') {
 				sortByPrice.sort((a, b) => a.price - b.price)
 			} else {
 				sortByPrice.sort((a, b) => b.price - a.price)
 			}
-
 			return {
 				...state, SneakersCopy: [...sortByPrice]
 			}
@@ -140,17 +137,13 @@ const rootReducer = (state = initialState, { type, payload }) => {
 				...state,
 				...payload,
 			}
-		//return Object.assign({}, state, payload);
-
-
+		
     case 'GET_CART_BD':
 			const filterDB = payload.filter(prodDB => state.productData.every(product => (prodDB.sneakerId !== product.sneakerId) || (prodDB.sneakerId === product.sneakerId && prodDB.size !== product.size)));
-
       return {
         ...state,
         productData: [...state.productData, ...filterDB],
       }
-
 
 		case REMOVE_ITEM_CART:
 			return {
@@ -165,7 +158,6 @@ const rootReducer = (state = initialState, { type, payload }) => {
 			}
 		//-------------------ADMIN------------------ADMIN--------------------ADMIN
 		case GET_ALL_USERS:
-
 			return {
 				...state,
 				users: payload
@@ -173,14 +165,12 @@ const rootReducer = (state = initialState, { type, payload }) => {
 
 		case DELETE_USER:
 			let user = [...state.users]
-
 			return {
 				...state,
 				users: user.filter(el => el.id !== payload)
 			}
 
 		case CREATE_MODEL:
-
 			return {
 				...state,
 				createModel: payload
@@ -193,37 +183,31 @@ const rootReducer = (state = initialState, { type, payload }) => {
 			}
 
 		case CREATE_CATEGORY:
-
 			return {
 				...state,
 				categories: payload
 			}
 
 		case DELETE_CATEGORY:
-
 			let category = state.categories
-
 			return {
 				...state,
 				categories: category.filter(el => el.id !== payload)
 			}
 
 		case GET_MODELS:
-
 			return {
 				...state,
 				getModels: payload
 			}
 
 		case GET_BRANDS:
-
 			return {
 				...state,
 				getBrands: payload
 			}
 
 		case GET_MATERIALS:
-
 			return {
 				...state,
 				getMaterials: payload
@@ -231,14 +215,12 @@ const rootReducer = (state = initialState, { type, payload }) => {
 
 
 		case GET_COLORS:
-
 			return {
 				...state,
 				getColors: payload
 			}
 
 		case GET_SIZES:
-
 			return {
 				...state,
 				getSizes: payload
@@ -248,21 +230,25 @@ const rootReducer = (state = initialState, { type, payload }) => {
 				...state,
 				getReviews: payload
 			}
+      
 		case GET_ROLE:
 			return{
 				...state,
 				getRole: payload
 			}
+      
 		case GET_TOKEN:
 			return{
 				...state,
 				getToken: payload
 			}
+      
 		case GET_USER:
 			return{
 				...state,
 				getUser: payload
 			}
+      
 		case RESET:
 			return{
 				...state,
@@ -278,7 +264,6 @@ const rootReducer = (state = initialState, { type, payload }) => {
 			}
 
 		case GET_ORDER_BY_ID:
-
 			return {
 				...state,
 				orderById: payload
