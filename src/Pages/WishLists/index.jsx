c6558452dfa5ab93ee366dd9edc5194251ccf610
avaldiviach <avaldiviach@gmail.com>
2022-06-05 02:35:53 -0500

    cambios

--- src/Pages/WishLists/index.jsx
+++ src/Pages/WishLists/index.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import WishList from "./WishList";
 import style from './WishLists.module.css';
@@ -7,23 +6,23 @@ function WishLists() {
 
   const productWishList = useSelector(state => state.wishlistData);
   const render = productWishList.filter(e => e.wishlisted);
-  const render2 = render.filter(d => d.wishlisted === true)
+
   return (
     <>
       <div className={style.page}>
         <h1>My Wishlist</h1>
       </div>
       {
-        render.length > 0
-          ? (
+        render.length === 0
+          ? (<div className={style.msg}><h3>Add products to your wishlist</h3></div>)
+          : (
             <div className={style.cards}>
               {
-                render2?.map(data => <WishList key={data.id} data={data} />)
+                render?.map(data => <WishList key={data.id} data={data} />)
 
               }
             </div>
           )
-          : (<div className={style.msg}><h3>Add products to your wishlist</h3></div>)
       }
     </>
   )
